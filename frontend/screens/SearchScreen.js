5982db2860cb8ca5a2599e50d01d35cd95af1bf3
gabrielbr619 <gabrielbr619@hotmail.com>
2024-08-07 14:40:59 -0300

    add SearchScreen.js

---
+++ frontend/screens/SearchScreen.js
@@ -0,0 +1,181 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text, TouchableOpacity } from "react-native";
+import { Button, CheckBox, Icon, Input, Slider } from "react-native-elements";
+
+const SearchScreen = ({ navigation }) => {
+  const [selectedPet, setSelectedPet] = useState(null);
+  const [selectedGender, setSelectedGender] = useState(null);
+  const [petSize, setPetSize] = useState(50);
+  const [location, setLocation] = useState("");
+
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    getUser();
+  }, []);
+
+  const getUser = async () => {
+    try {
+      const userData = await AsyncStorage.getItem("user");
+      const userDataParsed = JSON.parse(userData);
+      //   const token = await AsyncStorage.getItem('userToken');
+      //   setUserToken(token);
+      setUserData(userDataParsed);
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  const handleSearch = () => {
+    console.log({
+      selectedPet,
+      selectedGender,
+      petSize,
+      location,
+    });
+    // Navegar para a tela de resultados
+    navigation.navigate("Results", {
+      selectedPet,
+      selectedGender,
+      petSize,
+      location,
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>Bem vindo {userData.username},</Text>
+      <Text style={styles.title}>
+        Vamos te ajudar a encontrar o pet perfeito para você :)
+      </Text>
+
+      <Text style={styles.subtitle}>Qual pet você quer adotar?</Text>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          title="Gato"
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          checked={selectedPet === "Cat"}
+          onPress={() => setSelectedPet("Cat")}
+          containerStyle={styles.checkbox}
+        />
+        <CheckBox
+          title="Cachorro"
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          checked={selectedPet === "Dog"}
+          onPress={() => setSelectedPet("Dog")}
+          containerStyle={styles.checkbox}
+        />
+      </View>
+
+      <Text style={styles.subtitle}>Escolha o gênero do pet</Text>
+      <View style={styles.checkboxContainer}>
+        <CheckBox
+          title="Masculino"
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          checked={selectedGender === "Male"}
+          onPress={() => setSelectedGender("Male")}
+          containerStyle={styles.checkbox}
+        />
+        <CheckBox
+          title="Feminino"
+          checkedIcon="dot-circle-o"
+          uncheckedIcon="circle-o"
+          checked={selectedGender === "Female"}
+          onPress={() => setSelectedGender("Female")}
+          containerStyle={styles.checkbox}
+        />
+      </View>
+
+      <Text style={styles.subtitle}>Qual tamanho de pet?</Text>
+      <Slider
+        value={petSize}
+        onValueChange={(value) => setPetSize(value)}
+        minimumValue={35}
+        maximumValue={60}
+        step={1}
+        thumbTintColor="#fc9355"
+        minimumTrackTintColor="#fc9355"
+        maximumTrackTintColor="#fc9355"
+        style={styles.slider}
+      />
+      <Text
+        style={styles.sizeText}
+      >{`Tamanho: ${Math.round(petSize)} cm`}</Text>
+
+      <Text style={styles.subtitle}>
+        Compartilhe sua localização para encontrar pets próximos
+      </Text>
+      <Input
+        placeholder="R. Joaquim Nabuco, 2829 - Dionísio"
+        leftIcon={{
+          type: "font-awesome",
+          name: "map-marker",
+          color: "#fc9355",
+        }}
+        value={location}
+        onChangeText={setLocation}
+        containerStyle={styles.input}
+      />
+
+      <Button
+        title="Achar meu pet"
+        buttonStyle={styles.button}
+        onPress={handleSearch}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: "#f5f5f5",
+  },
+  welcome: {
+    fontSize: 18,
+    fontWeight: "bold",
+    marginBottom: 5,
+  },
+  title: {
+    fontSize: 16,
+    color: "#666",
+    marginBottom: 20,
+  },
+  subtitle: {
+    fontSize: 14,
+    fontWeight: "bold",
+    marginVertical: 10,
+  },
+  checkboxContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  checkbox: {
+    backgroundColor: "transparent",
+    borderWidth: 0,
+  },
+  slider: {
+    marginVertical: 10,
+  },
+  sizeText: {
+    textAlign: "center",
+    marginVertical: 10,
+    color: "#666",
+  },
+  input: {
+    marginTop: 10,
+  },
+  button: {
+    backgroundColor: "#fc9355",
+    borderRadius: 25,
+    paddingVertical: 10,
+    marginTop: 20,
+  },
+});
+
+export default SearchScreen;
--- frontend/screens/Tabs.js
+++ frontend/screens/Tabs.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Ionicons } from '@expo/vector-icons';
-import HomeScreen from './HomeScreen';
-import ProfileScreen from './ProfileScreen';
-import DiscoverScreen from './DiscoverScreen';
-import ChatsScreen from './ChatsScreen';
-import MessageScreen from './MessageScreen';
+import React from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createStackNavigator } from "@react-navigation/stack";
+import { Ionicons } from "@expo/vector-icons";
+import HomeScreen from "./HomeScreen";
+import ProfileScreen from "./ProfileScreen";
+import ChatsScreen from "./ChatsScreen";
+import MessageScreen from "./MessageScreen";
+import SearchScreen from "./SearchScreen";
 
 const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
@@ -14,44 +14,65 @@ const Stack = createStackNavigator();
 function ChatStack() {
   return (
     <Stack.Navigator>
-      <Stack.Screen name="ChatsStack" component={ChatsScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="Message" component={MessageScreen} options={{ headerShown: false }}/>
+      <Stack.Screen
+        name="ChatsStack"
+        component={ChatsScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Message"
+        component={MessageScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
 
-export default function Tabs({isPetOwner}) {
-console.log(isPetOwner)
-  if(isPetOwner){
-    console.log("entrou aq")
-    return(
+export default function Tabs({ isPetOwner }) {
+  console.log(isPetOwner);
+  if (isPetOwner) {
+    console.log("entrou aq");
+    return (
       <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
 
-          if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Discover') {
-            iconName = focused ? 'search' : 'search-outline';
-          } else if (route.name === 'Chats') {
-            iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
-          } else if (route.name === 'Profile') {
-            iconName = focused ? 'person' : 'person-outline';
-          }
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "Search") {
+              iconName = focused ? "search" : "search-outline";
+            } else if (route.name === "Chats") {
+              iconName = focused ? "chatbubbles" : "chatbubbles-outline";
+            } else if (route.name === "Profile") {
+              iconName = focused ? "person" : "person-outline";
+            }
 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-      })}
-      tabBarOptions={{
-        activeTintColor: '#fc9355',
-        inactiveTintColor: 'gray',
-      }}
-    >
-      <Tab.Screen name="Chats" component={ChatStack} options={{ headerShown: false, tabBarShowLabel: false }} />
-      <Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false, tabBarShowLabel: false }} />
-    </Tab.Navigator>
-    )
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "#fc9355",
+          inactiveTintColor: "gray",
+        }}
+      >
+        <Tab.Screen
+          name="Chats"
+          component={ChatStack}
+          options={{ headerShown: false, tabBarShowLabel: false }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ headerShown: false, tabBarShowLabel: false }}
+        />
+        <Tab.Screen
+          name="Search"
+          component={SearchScreen}
+          options={{ headerShown: false, tabBarShowLabel: false }}
+        />
+      </Tab.Navigator>
+    );
   }
   return (
     <Tab.Navigator
@@ -59,30 +80,44 @@ console.log(isPetOwner)
         tabBarIcon: ({ focused, color, size }) => {
           let iconName;
 
-          if (route.name === 'Home') {
-            iconName = focused ? 'home' : 'home-outline';
-          } else if (route.name === 'Discover') {
-            iconName = focused ? 'search' : 'search-outline';
-          } else if (route.name === 'Chats') {
-            iconName = focused ? 'chatbubbles' : 'chatbubbles-outline';
-          } else if (route.name === 'Profile') {
-            iconName = focused ? 'person' : 'person-outline';
+          if (route.name === "Home") {
+            iconName = focused ? "home" : "home-outline";
+          } else if (route.name === "Search") {
+            iconName = focused ? "search" : "search-outline";
+          } else if (route.name === "Chats") {
+            iconName = focused ? "chatbubbles" : "chatbubbles-outline";
+          } else if (route.name === "Profile") {
+            iconName = focused ? "person" : "person-outline";
           }
 
           return <Ionicons name={iconName} size={size} color={color} />;
         },
       })}
       tabBarOptions={{
-        activeTintColor: '#fc9355',
-        inactiveTintColor: 'gray',
+        activeTintColor: "#fc9355",
+        inactiveTintColor: "gray",
       }}
     >
-
-      
-      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false, tabBarShowLabel: false}} />
-      <Tab.Screen name="Discover" component={DiscoverScreen} options={{ headerShown: false, tabBarShowLabel: false }} />
-      <Tab.Screen name="Chats" component={ChatStack} options={{ headerShown: false, tabBarShowLabel: false }} />
-      <Tab.Screen name="Profile" component={ProfileScreen} options={{ headerShown: false, tabBarShowLabel: false }} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{ headerShown: false, tabBarShowLabel: false }}
+      />
+      <Tab.Screen
+        name="Search"
+        component={SearchScreen}
+        options={{ headerShown: false, tabBarShowLabel: false }}
+      />
+      <Tab.Screen
+        name="Chats"
+        component={ChatStack}
+        options={{ headerShown: false, tabBarShowLabel: false }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{ headerShown: false, tabBarShowLabel: false }}
+      />
     </Tab.Navigator>
   );
 }
