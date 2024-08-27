bdd4176dbfc45479aa825b8c761aa7aa9c3e8654
gabrielbr619 <gabrielbr619@hotmail.com>
2024-08-27 09:01:04 -0300

    add new route

--- backend/src/routes/petRoutes.js
+++ backend/src/routes/petRoutes.js
@@ -1,15 +1,23 @@
-const express = require('express');
-const { registerPet, deletePet, getRandomPet, userDislikePet } = require('../controllers/petController');
-const { authToken } = require('../middlewares/authToken');
-const upload = require('../config/multer');
+const express = require("express");
+const {
+  registerPet,
+  deletePet,
+  getRandomPet,
+  userDislikePet,
+  updatePetById,
+} = require("../controllers/petController");
+const { authToken } = require("../middlewares/authToken");
+const upload = require("../config/multer");
 const router = express.Router();
 
-router.get('/randomPet/:userId', authToken, getRandomPet);
+router.get("/randomPet/:userId", authToken, getRandomPet);
 
-router.post('/add', authToken, upload.array('images', 10), registerPet);
+router.post("/add", authToken, upload.array("pictures", 5), registerPet);
 
-router.put('/dislikePet', authToken, userDislikePet);
+router.put("/update", authToken, upload.array("pictures", 5), updatePetById);
 
-router.delete('/delete', authToken, deletePet);
+router.put("/dislikePet", authToken, userDislikePet);
+
+router.delete("/delete", authToken, deletePet);
 
 module.exports = router;
