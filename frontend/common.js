4cd298f038af0bcea0891cec711097552ac7e702
gabrielbr619 <gabrielbr619@hotmail.com>
2024-09-11 09:33:41 -0300

    feat: Update API_BASE_URL based on environment variable
    This commit updates the `API_BASE_URL` in the `common.js` file to be determined based on the `ENVIRONMENT` variable from the `@env` module. If the `ENVIRONMENT` is set to "production", the `API_BASE_URL` will be set to the value of `PROD_API`. Otherwise, it will be set to `${LOCALHOST}/api/`.

--- frontend/common.js
+++ frontend/common.js
@@ -1,9 +1,16 @@
 import { Platform } from "react-native";
-import { LOCALHOST_IOS_ANDROID, LOCALHOST_WEB } from "@env";
+import {
+  LOCALHOST_IOS_ANDROID,
+  LOCALHOST_WEB,
+  PROD_API,
+  ENVIRONMENT,
+} from "@env";
 
 const LOCALHOST =
   Platform.OS === "ios" || Platform.OS === "android"
     ? LOCALHOST_IOS_ANDROID
     : LOCALHOST_WEB;
 
-export const API_BASE_URL = `${LOCALHOST}/api/`;
+export const API_BASE_URL =
+  ENVIRONMENT === "production" ? PROD_API : `${LOCALHOST}/api/`;
+console.log(API_BASE_URL, LOCALHOST_IOS_ANDROID, LOCALHOST_WEB);
